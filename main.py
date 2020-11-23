9b96d7713b845c1673871da271726a70f4f2991d
ombe <h3236516@gmail.com>
2020-11-23 22:48:22 +0900

    오늘급식 명령어 추가

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import requests
 from bs4 import BeautifulSoup
 import os
+import datetime
 
 bot = commands.Bot(command_prefix='정발고 ')
 
@@ -20,6 +21,8 @@ async def 명령어(ctx):
         url='http://www.jeongbal.hs.kr/upload/2018/09/04/3ae8ad2fe586c0f6449248bbab811998.jpg')
     embed.add_field(name=' - 정발고 급식 M D',
                     value='M월 D일의 급식표를 불러옵니다.', inline=False)
+    embed.add_field(name=' - 정발고 오늘급식',
+                    value='오늘의 급식표를 불러옵니다.', inline=False)
     embed.add_field(name=' - 정발고 초대', value='이 봇의 초대 링크를 불러옵니다.', inline=False)
     await ctx.send(embed=embed)
 
@@ -63,8 +66,49 @@ async def 급식(ctx, m, d):
 
 
 @bot.command()
+async def 오늘급식(ctx):
+    await ctx.send('불러오는 중...')
+
+    m = datetime.datetime.now().month
+    d = datetime.datetime.now().day
+
+    req = requests.get(
+        f'http://www.jeongbal.hs.kr/wah/main/schoolmeal/view.htm?menuCode=38775&domain.year=2020&domain.month={m}&domain.day={d}')
+    con = req.content
+    html = BeautifulSoup(con, "html.parser")
+
+    try:
+        lunchList = html.find(
+            "div", {"class": "Schoolmeal_Cont_Cont_Cont"}).text
+    except:
+        embed = discord.Embed(title=f':warning: 정발고 {m}월 {d}일 급식표',
+                              description='불러오지 못했습니다.\n오늘이 주말이거나 급식이 나오지 않는 날인지 확인해주세요.', color=0xababab)
+        embed.set_thumbnail(
+            url='http://www.jeongbal.hs.kr/upload/2018/09/04/3ae8ad2fe586c0f6449248bbab811998.jpg')
+        await ctx.send(embed=embed)
+        return None
+    try:
+        cont2 = html.find_all("div", {"class": "Schoolmeal_Cont_Cont_2"})
+        allergyList = cont2[1].text
+    except:
+        embed = discord.Embed(
+            title=f':spoon: 정발고 {m}월 {d}일 급식표', description=f'{lunchList}', color=0xababab)
+        embed.set_thumbnail(
+            url='http://www.jeongbal.hs.kr/upload/2018/09/04/3ae8ad2fe586c0f6449248bbab811998.jpg')
+        await ctx.send(embed=embed)
+        return None
+
+    embed = discord.Embed(
+        title=f':spoon: 정발고 {m}월 {d}일 급식표', description=f'{lunchList}', color=0xababab)
+    embed.add_field(name=':bulb: 알레르기 정보', value=allergyList, inline=True)
+    embed.set_thumbnail(
+        url='http://www.jeongbal.hs.kr/upload/2018/09/04/3ae8ad2fe586c0f6449248bbab811998.jpg')
+    await ctx.send(embed=embed)
+
+
+@bot.command()
 async def 초대(ctx):
     await ctx.send('https://discord.com/oauth2/authorize?client_id=766501550687387659&scope=bot&permissions=2146954615')
 
 
-bot.run('NzY1NTU3MTM3ODMyNTQyMjA4.X4WipQ.I2m8RG7rsw9_Z63cPAMv7yeD58o')
+bot.run(os.environ['token'])
