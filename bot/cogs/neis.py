5f870cff9938e8d9f0cdb88af3141bee883f0972
YongChan Cho <h3236516@gmail.com>
2021-11-27 02:47:01 +0900

    feat(neis): add schedule, time_table command

--- bot/cogs/neis.py
+++ bot/cogs/neis.py
@@ -19,6 +19,20 @@ class NeisCog(Cog):
         embed = await self.neis.meal_embed(date)
         await msg.edit(embed=embed)
 
+    @commands.command(name="학사일정")
+    async def schedule(self, ctx: Context, date: Optional[str]) -> None:
+        msg = await ctx.send(embed=pleaseWait)
+        embed = await self.neis.schedule_embed(date)
+        await msg.edit(embed=embed)
+
+    @commands.command(name="시간표")
+    async def time_table(
+        self, ctx: Context, grade: int, class_nm: int, date: Optional[str]
+    ) -> None:
+        msg = await ctx.send(embed=pleaseWait)
+        embed = await self.neis.time_table_embed(grade, class_nm, date)
+        await msg.edit(embed=embed)
+
 
 def setup(bot: JeongBalBot) -> None:
     bot.add_cog(NeisCog(bot))
