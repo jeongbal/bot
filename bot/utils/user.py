70f00494549853b8607caff5edcdb6e831237c06
YongChan Cho <h3236516@gmail.com>
2021-11-28 22:08:46 +0900

    feat(utils): add user

---
+++ bot/utils/user.py
@@ -0,0 +1,11 @@
+from discord.embeds import Embed
+from bot.utils.database.mongo import Mongo
+
+
+class User:
+    def __init__(self, mongo: Mongo) -> None:
+        self.mongo = mongo
+
+    async def set_class_embed(self, user_id: int, grade: int, class_nm: int) -> Embed:
+        await self.mongo.set_user(user_id, grade, class_nm)
+        return Embed(title="반 정보 저장 완료")
