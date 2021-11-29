b8c5a4e7d622ab6fafa79cf94c8c2ccbef2f79f7
YongChan Cho <h3236516@gmail.com>
2021-11-29 12:43:57 +0900

    fix(user): too many args

--- bot/utils/user.py
+++ bot/utils/user.py
@@ -7,7 +7,7 @@ class User:
         self.mongo = mongo
 
     async def set_class_embed(self, user_id: int, grade: int, class_nm: int) -> Embed:
-        if await self.mongo.get_user(user_id, grade, class_nm):
+        if await self.mongo.get_user(user_id):
             await self.mongo.set_user(user_id, grade, class_nm)
         else:
             await self.mongo.initialize_user(user_id, grade, class_nm)
